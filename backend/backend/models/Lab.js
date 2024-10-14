bec90b7033efd9b3f8cd13bdca9a1798fdb3a4dd
Jaliya Herath <110586465+IT21801136@users.noreply.github.com>
2024-10-14 17:34:38 +0530

    Delete backend/backend directory

--- backend/backend/models/Lab.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const labSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  type: { type: String, required: true },
-  availability: [{ type: Date }]
-});
-
-module.exports = mongoose.model('Lab', labSchema);
--- backend/backend/routes/paymentRoutes.js
+++
@@ -1,8 +0,0 @@
-// backend/routes/paymentRoutes.js
-const express = require('express');
-const { processPayment } = require('../controllers/paymentController');
-const router = express.Router();
-
-router.post('/', processPayment);
-
-module.exports = router;
